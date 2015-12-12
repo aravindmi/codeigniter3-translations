78b5ff0186e4bde577b05b022f38815944a461aa
Aravind MI <miaravindh@gmail.com>
2015-12-12 10:00:08 +0530

    Added Malayalam Directory

---
+++ language/malayalam/calendar_lang.php
@@ -0,0 +1,55 @@
+<?php
+/**
+* System messages translation for CodeIgniter(tm)
+*
+* @author	CodeIgniter community
+* @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+* @license	http://opensource.org/licenses/MIT MIT License
+* @link	http://codeigniter.com
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['cal_su'] = 'ഞായര്‍';
+$lang['cal_mo'] = 'തിങ്കള്‍';
+$lang['cal_tu'] = 'ചൊവ്വ';
+$lang['cal_we'] = 'ബുധന്‍';
+$lang['cal_th'] = 'വ്യാഴം';
+$lang['cal_fr'] = 'വെള്ളി';
+$lang['cal_sa'] = 'ശനി';
+$lang['cal_sun'] = 'ഞായര്‍';
+$lang['cal_mon'] = 'തിങ്കള്‍';
+$lang['cal_tue'] = 'ചൊവ്വ';
+$lang['cal_wed'] = 'ബുധന്‍';
+$lang['cal_thu'] = 'വ്യാഴം';
+$lang['cal_fri'] = 'വെള്ളി';
+$lang['cal_sat'] = 'ശനി';
+$lang['cal_sunday'] = 'रविवार';
+$lang['cal_monday'] = 'सोमवार;';
+$lang['cal_tuesday'] = 'मंगलवार;';
+$lang['cal_wednesday'] = 'बुधवार;';
+$lang['cal_thursday'] = 'गुरुवार ;';
+$lang['cal_friday'] = 'शुक्रवार';
+$lang['cal_saturday'] = 'शनिवार';
+$lang['cal_jan'] = 'जनवरी';
+$lang['cal_feb'] = 'फ़रवरी';
+$lang['cal_mar'] = 'मार्च';
+$lang['cal_apr'] = 'अप्रैल';
+$lang['cal_may'] = 'मई';
+$lang['cal_jun'] = 'जून';
+$lang['cal_jul'] = 'जुलाई';
+$lang['cal_aug'] = 'अगस्त';
+$lang['cal_sep'] = 'सितंबर';
+$lang['cal_oct'] = 'अक्टूबर';
+$lang['cal_nov'] = 'नवंबर';
+$lang['cal_dec'] = 'दिसंबर';
+$lang['cal_january'] = 'जनवरी';
+$lang['cal_february'] = 'फ़रवरी';
+$lang['cal_march'] = 'मार्च';
+$lang['cal_april'] = 'अप्रैल';
+$lang['cal_mayl'] = 'मई';
+$lang['cal_june'] = 'जून';
+$lang['cal_july'] = 'जुलाई';
+$lang['cal_august'] = 'अगस्त';
+$lang['cal_september'] = 'सितंबर';
+$lang['cal_october'] = 'अक्टूबर';
+$lang['cal_november'] = 'नवंबर';
+$lang['cal_december'] = 'दिसंबर';
